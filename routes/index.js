e995440ed792ee2ec559746abeca951d8ab1efbc
zemeister23 <sauda2332@gmail.com>
2019-07-26 12:19:53 +0000

    sahifa rasm update

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'RedWolffffffff' });
+  res.render('index', { title: 'RedWolf' });
 });
 
 module.exports = router;
