352ab9f8a9aa30236c3bb1e4d9d0274589b3911c
zemeister23 <sauda2332@gmail.com>
2019-07-26 12:09:20 +0000

    yozuv3

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'RedWolf Corp.!!!' });
+  res.render('index', { title: 'RedWolffffffff' });
 });
 
 module.exports = router;
